1e662747e7603d2e125d53baa08c24a059fb2729
Douglas <doouglascarvalho92@gmail.com>
2021-03-02 18:08:20 -0300

    mudando os comentários das views.py

--- product/views.py
+++ product/views.py
@@ -43,13 +43,13 @@ class ProductFeaturedDetailView(DetailView):
     queryset = Product.objects.all().featured()
     template_name = "products/featured-detail.html"
 
+#View que irá mostrar a imagem usando a Slug do produto
 class ProductDetailSlugView(DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_object(self, *args, **kwargs):
         slug = self.kwargs.get('slug')
-        #instance = get_object_or_404(Product, slug = slug, active = True)
         try:
             instance = Product.objects.get(slug = slug, active = True)
         except Product.DoesNotExist:
