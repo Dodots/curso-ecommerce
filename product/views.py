de928ed239f9e0364c3965b2d140a5d7aaca180b
Douglas <doouglascarvalho92@gmail.com>
2021-03-03 20:04:52 -0300

    comentando as views que não estao sendo usadas

--- product/views.py
+++ product/views.py
@@ -10,39 +10,6 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
     
-    def get_context_data(self, *args, **kwargs):
-        context = super(ProductListView, self).get_context_data(*args, **kwargs)
-        print(context)
-        return context
-
-#View que irá listar os produtos em destaque 
-class ProductFeaturedListView(ListView):
-    template_name = "products/list.html"
-    
-    def get_queryset(self, *args, **kwargs):
-        return Product.objects.featured()
-
-#View que irá mostrar um unico produto
-class ProductDetailView(DetailView):
-    template_name = "products/detail.html"
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-        print(context)
-        return context
-    
-    def get_object(self, *args, **kwargs):
-        pk = self.kwargs.get('pk')
-        instance = Product.objects.get_by_id(pk)
-        if instance is None:
-            raise Http404("Esse produto não existe!")
-        return instance
-
-#View que irá mostrar um unico produto, mas só se ele estiver em destaque
-class ProductFeaturedDetailView(DetailView):
-    queryset = Product.objects.all().featured()
-    template_name = "products/featured-detail.html"
-
 #View que irá mostrar a imagem usando a Slug do produto
 class ProductDetailSlugView(DetailView):
     queryset = Product.objects.all()
@@ -58,3 +25,32 @@ class ProductDetailSlugView(DetailView):
             qs = Product.objects.filter(slug = slug, active = True)
             instance =  qs.first()
         return instance
+
+#View que irá listar os produtos em destaque 
+#class ProductFeaturedListView(ListView):
+#    template_name = "products/list.html"
+    
+#    def get_queryset(self, *args, **kwargs):
+#        return Product.objects.featured()
+
+#View que irá mostrar um unico produto
+#class ProductDetailView(DetailView):
+#    template_name = "products/detail.html"
+
+#    def get_context_data(self, *args, **kwargs):
+#        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+#        print(context)
+#        return context
+    
+#    def get_object(self, *args, **kwargs):
+#        pk = self.kwargs.get('pk')
+#        instance = Product.objects.get_by_id(pk)
+#        if instance is None:
+#            raise Http404("Esse produto não existe!")
+#        return instance
+
+#View que irá mostrar um unico produto, mas só se ele estiver em destaque
+#class ProductFeaturedDetailView(DetailView):
+#    queryset = Product.objects.all().featured()
+#    template_name = "products/featured-detail.html"
+
