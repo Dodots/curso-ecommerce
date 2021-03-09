7967c62ce36b14f1a80413c888f2e47319ebfc48
Douglas <doouglascarvalho92@gmail.com>
2021-03-09 15:43:47 -0300

    criando a url, view e adicionando eles no navbar. (search)

--- search/views.py
+++ search/views.py
@@ -3,8 +3,18 @@ from django.views.generic import ListView
 from product.models import Product
 
 class SearchProductView(ListView):
-    template_name = "products/list.html"
+    template_name = "search/view.html"
 
+    #Função criada para verificar se tem ou não o valor no search
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        #SearchQuery.objects.create(query=query)
+        return context
+        
+    #Função para fazer a pesquisa com o valor colocado no search
+    # se não tiver valor ele aparecerá os valores em destaque.    
     def get_queryset(self, *args, **kargs):
         request = self.request
         result = request.GET
